32201a3ce36a375370870bcece26a41229ec8689
Talha O <talha43gs@gmail.com>
2024-01-03 19:31:52 +0000

    Local1 ve Local2 eklendi

---
+++ src/Local1.java
@@ -0,0 +1,9 @@
+public class Local1 {
+
+    public static void main(String[] args) {
+        System.out.println("Git Test");
+        System.out.println("Yeni satir eklendi!");
+        System.out.println("Yeni test");
+
+    }
+}
---
+++ src/Local2.java
@@ -0,0 +1,7 @@
+public class Local2 {
+
+    public static void main(String[] args) {
+        System.out.println("test1");
+        System.out.println("test2");
+    }
+}
