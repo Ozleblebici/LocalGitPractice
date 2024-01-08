519148c40a20fa4d8023fa9c443b8348c3bcac1f
Talha OZLEBLEBICI <talha43gs@gmail.com>
2024-01-08 19:35:51 +0000

    test1 updated

--- src/Test1.java
+++ src/Test1.java
@@ -1,2 +1,7 @@
 public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("TEST12");
+        System.out.println("TEST13");
+        System.out.println("TEST14");
+    }
 }
