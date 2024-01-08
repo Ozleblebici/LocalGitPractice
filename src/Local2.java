68d17050140f3f46f537ec2ad1404e12dee70015
Talha OZLEBLEBICI <talha43gs@gmail.com>
2024-01-08 19:34:17 +0000

    test1 uploaded

--- src/Local2.java
+++
@@ -1,9 +0,0 @@
-public class Local2 {
-
-    public static void main(String[] args) {
-        System.out.println("test1");
-        System.out.println("test2");
-        System.out.println("test3");
-        System.out.println("Talha branch in da eklenen code!!");
-    }
-}
--- src/Talha.java
+++
@@ -1,7 +0,0 @@
-public class Talha {
-    public static void main(String[] args) {
-        System.out.println("Talha branch in da eklenen code!!, HATTA BURAYI DA DEGISTIRDIK REMOTE DAN!!!");
-        // YENI SATIRLAR EKLENDI!!!
-        System.out.println("Talha branch ina GITHUB REMOTE dan eklemeler yaptim...!!");
-    }
-}
